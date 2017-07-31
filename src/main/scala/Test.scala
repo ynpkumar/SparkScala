298fec45c622ea93a892fb8162c439f66f33056f
nagay <naga.yeemani@gmail.com>
2017-07-30 23:20:21 -0700

    Resolved merge conflicts

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -3,7 +3,6 @@ object Test {
   def main ( args : Array[String]): Unit =
   {
 <<<<<<< HEAD
-    print("Hello from ynpbranch")
 =======
     print("hello Master")
 >>>>>>> ea7e704fbc4786e86ee1d89aeeb468f6f7104ea2
